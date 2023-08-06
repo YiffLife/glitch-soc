1769931ec284e423880f49a9e2fbd26bddb0a31d
Sky <41547235+manedfolf@users.noreply.github.com>
2023-08-06 10:37:02 -0500

    update common.scss and rack_attack rate limits
    increased rate limits and minor adjustments to common.scss in glitch theme

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -66,7 +66,7 @@ class Rack::Attack
     IpBlock.blocked?(req.remote_ip)
   end
 
-  throttle('throttle_authenticated_api', limit: 1_500, period: 5.minutes) do |req|
+  throttle('throttle_authenticated_api', limit: 2_500, period: 5.minutes) do |req|
     req.authenticated_user_id if req.api_request?
   end
 
@@ -78,11 +78,11 @@ class Rack::Attack
     req.throttleable_remote_ip if req.api_request? && req.unauthenticated?
   end
 
-  throttle('throttle_api_media', limit: 30, period: 30.minutes) do |req|
+  throttle('throttle_api_media', limit: 300, period: 30.minutes) do |req|
     req.authenticated_user_id if req.post? && req.path.match?(%r{\A/api/v\d+/media\z}i)
   end
 
-  throttle('throttle_media_proxy', limit: 30, period: 10.minutes) do |req|
+  throttle('throttle_media_proxy', limit: 300, period: 10.minutes) do |req|
     req.throttleable_remote_ip if req.path.start_with?('/media_proxy')
   end
 
